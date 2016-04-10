d5781be7bee929669f257274b2ee3562bb262ced
rey <rey.8d01@yandex.ru>
2016-04-11 01:59:05 +0500

    add serialize OrderItem

--- api_market/backend/serializers.py
+++ api_market/backend/serializers.py
@@ -1,6 +1,6 @@
 """Сериализаторы для API."""
 from rest_framework import serializers
-from .models import Item, Order, User
+from .models import Item, Order, User, OrderItem
 
 
 class ItemSerializer(serializers.ModelSerializer):
@@ -15,9 +15,20 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('id', 'email', 'first_name', 'last_name', 'sex', 'address')
 
 
+class OrderItemSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField(source='item.id')
+    title = serializers.ReadOnlyField(source='item.title')
+    amount = serializers.ReadOnlyField(source='item.amount')
+
+    class Meta:
+        model = OrderItem
+        fields = ('id', 'title', 'amount', 'count_item')
+
+
 class OrderSerializer(serializers.ModelSerializer):
     user = UserSerializer(required=True)
-    items = ItemSerializer(many=True)  # Связь для М-М
+    # items = ItemSerializer(many=True)  # Связь для М-М
+    items = OrderItemSerializer(source='orderitem_set', many=True)  # Связь для М-М через сериализатор таблицы-связки для получения доп инфы
 
     class Meta:
         model = Order
